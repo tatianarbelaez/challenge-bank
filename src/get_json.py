1522f0165e87ac288a141f1e995c8634b6427dbc
tatianarbelaez <85839819+tatianarbelaez@users.noreply.github.com>
2022-09-06 23:42:05 -0500

    changes

--- src/get_json.py
+++ src/get_json.py
@@ -1,5 +1,3 @@
-
-import json
 import requests
 
 for day in range(1, 32):
@@ -10,5 +8,9 @@ for day in range(1, 32):
         day_string = str(day)
     result_string = requests.get('https://api.tvmaze.com/schedule/web?date=2020-12-' + day_string).text
     with open('../json/json_data_' + day_string + '.json', 'w') as outfile:
-       json.dump(result_string, outfile)
+       outfile.write(result_string)
+
+
+
+
 
